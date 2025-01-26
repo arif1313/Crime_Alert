54133cc6f36ed31efae20c7f88460801d19de66c
arif1313 <arifhossen13131@gmail.com>
2025-01-27 03:31:38 +0600

    first

---
+++ src/pages/Home/Banar/Banar.jsx
@@ -0,0 +1,11 @@
+import pic1 from '../../../assets/home/pics/crimepic.jpg'
+
+function Banar() {
+  return (
+    <div>
+        <img className='w-full h-screen' src={pic1} alt="" />
+    </div>
+  )
+}
+
+export default Banar
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Banar from "./Banar/Banar"
+
+
+const Home = () => {
+  return (
+    <div><Banar></Banar></div>
+  )
+}
+
+export default Home
