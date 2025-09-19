3507ac54fad9d24191182e2c1c7bd8d9083ac501
arif1313 <arifhossen13131@gmail.com>
2025-09-20 03:59:45 +0600

    map good

--- src/pages/Home/CrimeHeatmap/CrimeHeatmap.jsx
+++ src/pages/Home/CrimeHeatmap/CrimeHeatmap.jsx
@@ -1,73 +1,49 @@
 import { useMemo } from "react";
 import { GoogleMap, LoadScript, HeatmapLayer, Marker } from "@react-google-maps/api";
-import PropTypes from "prop-types";
 
 const mapContainerStyle = { width: "100%", height: "500px" };
-const center = { lat: 23.8103, lng: 90.4125 }; // Dhaka center
 const dhakaBounds = { north: 23.99, south: 23.6, west: 90.2, east: 90.55 };
 
 const CrimeHeatmap = () => {
   // --- Manual reports inside Dhaka ---
   const manualReports = useMemo(
     () => [
-      {
-        _id: "1",
-        reportTitle: "Report 1",
-        reportType: "Theft",
-        crimeDate: "2025-09-14",
-        lat: 23.8100, // near central Dhaka
-        lng: 90.4120,
-      },
-      {
-        _id: "2",
-        reportTitle: "Report 2",
-        reportType: "Assault",
-        crimeDate: "2025-09-14",
-        lat: 23.7900, // Mirpur area
-        lng: 90.3580,
-      },
-      {
-        _id: "3",
-        reportTitle: "Report 3",
-        reportType: "Vandalism",
-        crimeDate: "2025-09-14",
-        lat: 23.7500, // Gulshan area
-        lng: 90.4200,
-      },
-      {
-        _id: "4",
-        reportTitle: "Report 4",
-        reportType: "Theft",
-        crimeDate: "2025-09-14",
-        lat: 23.7700, // Dhanmondi area
-        lng: 90.4000,
-      },
-      {
-        _id: "5",
-        reportTitle: "Report 5",
-        reportType: "Assault",
-        crimeDate: "2025-09-14",
-        lat: 23.8200, // Uttara area
-        lng: 90.3700,
-      },
+      { _id: "1", reportTitle: "Report 1", reportType: "Theft", crimeDate: "2025-09-14", lat: 23.7980, lng: 90.4010 }, // Banani
+      { _id: "2", reportTitle: "Report 2", reportType: "Assault", crimeDate: "2025-09-14", lat: 23.8050, lng: 90.3900 }, // Banani
+      { _id: "3", reportTitle: "Report 3", reportType: "Vandalism", crimeDate: "2025-09-14", lat: 23.8230, lng: 90.3740 }, // Mirpur
+      { _id: "4", reportTitle: "Report 4", reportType: "Theft", crimeDate: "2025-09-14", lat: 23.8170, lng: 90.3820 }, // Mirpur
+      { _id: "5", reportTitle: "Report 5", reportType: "Assault", crimeDate: "2025-09-14", lat: 23.8105, lng: 90.3950 }  // Between Banani & Mirpur
     ],
     []
   );
 
+  console.log("Manual Reports:", manualReports);
+
   // Heatmap data
   const heatmapData = useMemo(() => {
     if (!window.google) return [];
-    return manualReports.map((report) => new window.google.maps.LatLng(report.lat, report.lng));
+    const data = manualReports.map((report) => new window.google.maps.LatLng(report.lat, report.lng));
+    console.log("Heatmap Data:", data);
+    return data;
+  }, [manualReports]);
+
+  console.log("Google Maps Loaded:", !!window.google);
+
+  // Calculate map center dynamically
+  const center = useMemo(() => {
+    if (!manualReports.length) return { lat: 23.8103, lng: 90.4125 };
+    const latSum = manualReports.reduce((sum, r) => sum + r.lat, 0);
+    const lngSum = manualReports.reduce((sum, r) => sum + r.lng, 0);
+    const c = { lat: latSum / manualReports.length, lng: lngSum / manualReports.length };
+    console.log("Map Center:", c);
+    return c;
   }, [manualReports]);
 
   return (
     <div>
       <h2>Crime Heatmap - Dhaka</h2>
 
-      <LoadScript
-        googleMapsApiKey="AIzaSyA_LUMMGjMq7asPjAPUdpZI-UVgyoS98GY"
-        libraries={["visualization"]}
-      >
+      <LoadScript googleMapsApiKey="AIzaSyA6LfNv2yErM8MaC7YvxDgL8af7TxrYSFE" libraries={["visualization"]}>
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
           zoom={12}
@@ -83,34 +59,30 @@ const CrimeHeatmap = () => {
             options={{ radius: 25, opacity: 0.6, dissipating: true }}
           />
 
-          {manualReports.map((report) => (
-            <Marker
-              key={report._id}
-              position={{ lat: report.lat, lng: report.lng }}
-              onClick={() =>
-                alert(
-                  `Crime: ${report.reportType}\nTitle: ${report.reportTitle}\nDate: ${report.crimeDate}`
-                )
-              }
-            />
-          ))}
+          {manualReports.map((report, index) => {
+            console.log("Rendering Marker:", report);
+            return (
+              <Marker
+                key={report._id}
+                position={{ lat: report.lat, lng: report.lng }}
+                icon={{
+                  url:
+                    index === 0
+                      ? "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" // first report blue
+                      : "http://maps.google.com/mapfiles/ms/icons/red-dot.png", // others red
+                }}
+                onClick={() =>
+                  alert(
+                    `Crime: ${report.reportType}\nTitle: ${report.reportTitle}\nDate: ${report.crimeDate}`
+                  )
+                }
+              />
+            );
+          })}
         </GoogleMap>
       </LoadScript>
     </div>
   );
-};
-
-CrimeHeatmap.propTypes = {
-  reports: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string,
-      reportTitle: PropTypes.string,
-      reportType: PropTypes.string,
-      crimeDate: PropTypes.string,
-      lat: PropTypes.number,
-      lng: PropTypes.number,
-    })
-  ),
 };
 
 export default CrimeHeatmap;
